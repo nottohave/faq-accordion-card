85564c81d1d9398421b92c9bd78d86df73b7bd93
nottohave <nottohave@gmail.com>
2021-08-19 22:29:27 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var answer = document.getElementsByClassName("answer");
+    if (answer.style.display === "none") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+  }
