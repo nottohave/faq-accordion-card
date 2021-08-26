1bf6471d192fba72f04a90eba55379f864541c51
nottohave <nottohave@gmail.com>
2021-08-25 21:09:14 -0700

    update

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-// working on active element, tune in tmr
-// var members = document.querySelector(".members");
-// var hrMargin = document.querySelector("#pageHr1");
+// make an array 1 2 3 4
+// make an if statement to see if you can check for a button that 
+// has the onlick event that has active class
+// if it is active, does it has the id of 1? if it does, 
+// can you display the div? inside this, add in the if clause if 
+// the display is block, change display to none.
 
-// var mamximum = document.querySelector(".maximum");
-// var hrMargin2 = document.querySelector("#pageHr2");
-
-// var forgotPw = document.querySelector(".forgotPw");
-// var hrMargin3 = document.querySelector("#pageHr3");
-
-// var subs = document.querySelector(".subs");
-// var hrMargin4 = document.querySelector("#pageHr4");
-
-// var support = document.querySelector(".support");
-// var hrMargin5 = document.querySelector("#pageHr5");
-
-var buttons = document.querySelectorAll("button");
-var check = document.getElementById("1");
 
 buttons.forEach(button => {
     button.addEventListener("click", function() {
@@ -24,67 +13,8 @@ buttons.forEach(button => {
         });
 
         button.classList.add("active");
-        showHide();
+
     })
 })
 
-//seperate function to show/hide content on click element.
-function showHide() {
 
-    if (answer.style.display === "none") {
-        answer.style.display = "block";
-    } else {
-        answer.style.display = "none";
-    }
-}
-
-// function displayMembers() {
-//     if (members.style.display === "none") {
-//         hrMargin2.style.margin = "0rem 0rem 1rem 0rem";
-//         members.style.display = "block";
-//     } else {
-//         members.style.display = "none";
-//         hrMargin.style.margin = "1rem 0rem 1rem 0rem";
-//     }
-// }
-// function displayMaximum() {
-//     if (mamximum.style.display === "none") {
-//         hrMargin2.style.margin = "0rem 0rem 1rem 0rem";
-//         mamximum.style.display = "block";
-//     } else {
-//         mamximum.style.display = "none";
-//         hrMargin2.style.margin = "1rem 0rem 1rem 0rem";
-//     }
-// }
-// function displayForgotPw() {
-//     if (forgotPw.style.display === "none") {
-//         hrMargin3.style.margin = "0rem";
-//         forgotPw.style.display = "block";
-//     } else {
-//         forgotPw.style.display = "none";
-//         hrMargin3.style.margin = "1rem 0rem 1rem 0rem";
-//     }
-// }
-// function displaySubs() {
-//     if (subs.style.display === "none") {
-//         hrMargin4.style.margin = "0rem";
-//         subs.style.display = "block";
-//     } else {
-//         subs.style.display = "none";
-//         hrMargin4.style.margin = "1rem 0rem 1rem 0rem";
-//     }
-// }
-
-// let cardHeight = document.querySelector(".card");
-// function displaySupport() {
-//     if (support.style.display === "none") {
-//         cardHeight.style.height = "550px";
-//         hrMargin5.style.margin = "0rem";
-//         support.style.display = "block";
-//     } else {
-//         support.style.display = "none";
-//         hrMargin5.style.margin = "1rem 0rem 1rem 0rem";
-//         cardHeight.style.height = "500px";
-
-//     }
-// }
