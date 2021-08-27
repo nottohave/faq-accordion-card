d57122fffac6d1f84137dde0dfaae03900c93cdb
nottohave <nottohave@gmail.com>
2021-08-26 22:32:02 -0700

    Looking for answer

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
 // if it is active, does it has the id of 1? if it does, 
 // can you display the div? inside this, add in the if clause if 
 // the display is block, change display to none.
-
+var array = [1, 2, 3, 4];
+var buttons = document.querySelectorAll("button")
+var activeBtn = document.getElementsByClassName("arrow active");
+var activeQA = document.get
 
 buttons.forEach(button => {
     button.addEventListener("click", function() {
@@ -13,8 +16,14 @@ buttons.forEach(button => {
         });
 
         button.classList.add("active");
-
+        active()
     })
 })
 
-
+function active() {
+    let children = activeBtn.children
+    if (activeBtn != null) {
+        
+    } 
+    
+}
