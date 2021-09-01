b44112be02222434ffaf1f9a31384d52362e3301
nottohave <nottohave@gmail.com>
2021-08-31 21:28:58 -0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+// Declare variables for setting active button and show/display contents
 var buttons = document.querySelectorAll("button");
-var activeBtn = document.getElementsByClassName("arrow active");
 
 var answer1 = document.querySelector(".answer1");
 var answer2 = document.querySelector(".answer2");
@@ -7,6 +7,14 @@ var answer3 = document.querySelector(".answer3");
 var answer4 = document.querySelector(".answer4");
 var answer5 = document.querySelector(".answer5");
 
+// Declare variables for answer to reset padding upon click.
+var answer1child = document.querySelector(".child1");
+var answer2child = document.querySelector(".child2");
+var answer3child = document.querySelector(".child3");
+var answer4child = document.querySelector(".child4");
+var answer5child = document.querySelector(".child5");
+
+// Add and remove active class when clicked
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         buttons.forEach(function(btn) {
@@ -17,9 +25,11 @@ buttons.forEach(button => {
     })
 })
 
+// Hide/show answer to each question upon clicking
 function faq1() {
     if (answer1.style.display === "none") {
         answer1.style.display = "block";
+        answer1child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
         answer1.style.display = "none";
     }
@@ -28,6 +38,7 @@ function faq1() {
 function faq2() {
     if (answer2.style.display === "none") {
         answer2.style.display = "block";
+        answer2child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
         answer2.style.display = "none";
     }
@@ -36,11 +47,13 @@ function faq2() {
 function faq3() {
     if (answer3.style.display === "none") {
         answer3.style.display = "block";
+        answer3child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
         answer3.style.display = "none";
     }
 }
 
+// Active 4th answer from top to bottom will hide the other three answers on top.
 function faq4() {
     let firstAns = answer1.style.display;
     let secondAns = answer2.style.display;
@@ -52,6 +65,7 @@ function faq4() {
     };
     if (answer4.style.display === "none") {
         answer4.style.display = "block";
+        answer4child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
         answer4.style.display = "none";
     }
@@ -60,6 +74,7 @@ function faq4() {
 function faq5() {
     if (answer5.style.display === "none") {
         answer5.style.display = "block";
+        answer5child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
         answer5.style.display = "none";
     }
