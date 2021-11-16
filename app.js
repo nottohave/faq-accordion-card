eff0a67d3668c9c5c71b6d312ee4265d80bbb36e
nottohave <nottohave@gmail.com>
2021-11-15 18:35:06 -0800

    updates

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Declare variables for setting active button and show/display contents
 var buttons = document.querySelectorAll("button");
 
-var answer1 = document.querySelector(".answer1");
-var answer2 = document.querySelector(".answer2");
-var answer3 = document.querySelector(".answer3");
-var answer4 = document.querySelector(".answer4");
-var answer5 = document.querySelector(".answer5");
+var ans1 = document.querySelector(".ans1");
+var ans2 = document.querySelector(".ans2");
+var ans3 = document.querySelector(".ans3");
+var ans4 = document.querySelector(".ans4");
+var ans5 = document.querySelector(".ans5");
 
 // Add and remove active class when clicked
 buttons.forEach(button => {
@@ -20,55 +20,55 @@ buttons.forEach(button => {
 
 // Hide/show answer to each question upon clicking
 function faq1() {
-    if (answer1.style.display === "none") {
-        answer1.style.display = "block";
-        answer1child.style.padding = "0.5rem 0rem 0.3rem 0rem";
+    if (ans1.style.display === "none") {
+        ans1.style.display = "block";
+        ans1child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
-        answer1.style.display = "none";
+        ans1.style.display = "none";
     }
 }
 
 function faq2() {
-    if (answer2.style.display === "none") {
-        answer2.style.display = "block";
-        answer2child.style.padding = "0.5rem 0rem 0.3rem 0rem";
+    if (ans2.style.display === "none") {
+        ans2.style.display = "block";
+        ans2child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
-        answer2.style.display = "none";
+        ans2.style.display = "none";
     }
 }
 
 function faq3() {
-    if (answer3.style.display === "none") {
-        answer3.style.display = "block";
-        answer3child.style.padding = "0.5rem 0rem 0.3rem 0rem";
+    if (ans3.style.display === "none") {
+        ans3.style.display = "block";
+        ans3child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
-        answer3.style.display = "none";
+        ans3.style.display = "none";
     }
 }
 
 // Active 4th answer from top to bottom will hide the other three answers on top.
 function faq4() {
-    let firstAns = answer1.style.display;
-    let secondAns = answer2.style.display;
-    let thirdAns = answer3.style.display;
+    let firstAns = ans1.style.display;
+    let secondAns = ans2.style.display;
+    let thirdAns = ans3.style.display;
     if (firstAns && secondAns && thirdAns === "block" ) {
         faq1();
         faq2();
         faq3();
     };
-    if (answer4.style.display === "none") {
-        answer4.style.display = "block";
-        answer4child.style.padding = "0.5rem 0rem 0.3rem 0rem";
+    if (ans4.style.display === "none") {
+        ans4.style.display = "block";
+        ans4child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
-        answer4.style.display = "none";
+        ans4.style.display = "none";
     }
 }
 
 function faq5() {
-    if (answer5.style.display === "none") {
-        answer5.style.display = "block";
-        answer5child.style.padding = "0.5rem 0rem 0.3rem 0rem";
+    if (ans5.style.display === "none") {
+        ans5.style.display = "block";
+        ans5child.style.padding = "0.5rem 0rem 0.3rem 0rem";
     } else {
-        answer5.style.display = "none";
+        ans5.style.display = "none";
     }
 }
